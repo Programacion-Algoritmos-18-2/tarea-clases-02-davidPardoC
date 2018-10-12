5bf160dc8fd918bc4528b3f402b8ea69b87e965a
David Pardo <pardodavid10@gmail.com>
2018-10-12 10:24:33 -0500

    Ejercicio 2

---
+++ Ejercicio 2/src/ejercicio/pkg2/Precio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg2;
+
+/**
+ *
+ * @author David Pardo
+ */
+public class Precio {
+    double euros;
+    public double obtener(){
+    return 0;
+    }
+    public void agragar(double x){
+    }
+}
